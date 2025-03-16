318d6269fa105c2a9966b140354dcf21fd86ea49
Badescu Petre Cosmin <badescupetrecosmin@gmail.com>
2025-03-16 17:36:08 +0200

    test

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,18 @@
-"use client";
-import { Market } from "./market";
-function Dashboard() {
-  return (
-    <>
-      <h1>With narket</h1>
-      <Market />
-    </>
-  );
+import { fetchWithCustomHeaders } from "../libs/headers";
+
+async function Dashboard() {
+  let data;
+  try {
+    const response = await fetchWithCustomHeaders(
+      "https://yahoo-finance15.p.rapidapi.com/api/v1/markets/stock/history?symbol=SXRV.DE&interval=1mo&diffandsplits=false"
+    );
+
+    data = await response.json();
+  } catch (er) {
+    console.error(er);
+  }
+
+  return <p>{data?.meta?.processedTime}</p>;
 }
 
 export default Dashboard;
