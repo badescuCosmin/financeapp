d2147b40780704f93c3716e135dca71e08f45dca
Badescu Petre Cosmin <badescupetrecosmin@gmail.com>
2025-03-16 17:09:07 +0200

    add server page

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,8 +1,13 @@
 import { fetchWithCustomHeaders } from "@/app/libs/headers";
 
 export const GET = async (request: Request) => {
+  const url = new URL(request.url);
+  const symbol = url.searchParams.get("symbol") || "SXRV.DE";
+  const interval = url.searchParams.get("interval") || "1mo";
+  const diffAndSplits = url.searchParams.get("diffandsplits") || "false";
+
   const response = await fetchWithCustomHeaders(
-    `https://yahoo-finance15.p.rapidapi.com/api/v1/markets/stock/history?symbol=SXRV.DE&interval=1mo&diffandsplits=false`
+    `https://yahoo-finance15.p.rapidapi.com/api/v1/markets/stock/history?symbol=${symbol}&interval=${interval}&diffandsplits=${diffAndSplits}`
   );
   const data = await response.json();
 
