3d244b10cae1cae4dcbf8b68bcf89ffa24534571
Badescu Petre Cosmin <badescupetrecosmin@gmail.com>
2025-03-16 17:14:33 +0200

    fix

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,16 @@
 import { fetchWithCustomHeaders } from "../libs/headers";
 
 async function Dashboard() {
-  const response = await fetchWithCustomHeaders(
-    "https://yahoo-finance15.p.rapidapi.com/api/v1/markets/stock/history?symbol=SXRV.DE&interval=1mo&diffandsplits=false"
-  );
+  let data;
+  try {
+    const response = await fetchWithCustomHeaders(
+      "https://yahoo-finance15.p.rapidapi.com/api/v1/markets/stock/history?symbol=SXRV.DE&interval=1mo&diffandsplits=false"
+    );
 
-  const data = await response.json();
-
-  console.log(data, "data");
+    data = await response.json();
+  } catch (er) {
+    console.error(er);
+  }
 
   return <p>{data?.meta?.processedTime}</p>;
 }
