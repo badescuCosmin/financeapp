f36e813fdafef45f2437db7519a1e33b0033535f
Badescu Petre Cosmin <badescupetrecosmin@gmail.com>
2025-03-18 13:38:01 +0200

    test error

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,23 +1,21 @@
+import { Suspense } from "react";
 import { ClientComponent } from "../clientC";
 
 import { fetchWithCustomHeaders } from "../libs/headers";
 
 async function Dashboard() {
-  let data;
-  try {
-    const response = await fetchWithCustomHeaders(
-      "https://yahoo-finance15.p.rapidapi.com/api/v1/markets/stock/history?symbol=SXRV.DE&interval=1mo&diffandsplits=false",
-      { next: { revalidate: 60 } }
-    );
+  const response = await fetchWithCustomHeaders(
+    "https://yahoo-finance15.p.rapidapi.scom/api/v1/markets/stock/history?symbol=SXRV.DE&interval=1mo&diffandsplits=false",
+    { next: { revalidate: 60 } }
+  );
 
-    data = await response.json();
-  } catch (er) {
-    console.error(er);
-  }
-
+  const data = await response.json();
   return (
     <div>
-      <p>{data?.meta?.processedTime}</p>
+      <Suspense fallback={<p>Loading...</p>}>
+        <p>{data?.meta?.processedTime}</p>
+      </Suspense>
+
       <ClientComponent />
     </div>
   );
