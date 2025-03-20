7992c703571416841cce92f0f7f82ec8d8ed06f6
Badescu Petre Cosmin <badescupetrecosmin@gmail.com>
2025-03-20 12:07:18 +0200

    add chart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
 import Chart from "./components/chart";
-import { fetchWithCustomHeaders } from "./libs/headers";
+import { mockData } from "./libs/mockData";
 
 export type Response = { date_utc: number; close: number };
 export default async function Home() {
-  const data = await fetchWithCustomHeaders(
-    `https://yahoo-finance15.p.rapidapi.com/api/v1/markets/stock/history?symbol=VWCE.DE&interval=1mo&diffandsplits=false`
-  );
-  const response = (await data.json()) as {
-    body: Record<string, Response>;
-    meta: Record<string, string>;
-  };
-  const mapData = Object.values(response.body).map((v) => ({
+  // const data = await fetchWithCustomHeaders(
+  //   `https://yahoo-finance15.p.rapidapi.com/api/v1/markets/stock/history?symbol=VWCE.DE&interval=1mo&diffandsplits=false`
+  // );
+  // const response = (await data.json()) as {
+  //   body: Record<string, Response>;
+  //   meta: Record<string, string>;
+  // };
+  const mapData = Object.values(mockData.body).map((v) => ({
     date_utc: v.date_utc,
     close: v.close,
   }));
