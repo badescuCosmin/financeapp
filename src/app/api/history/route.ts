a74ac4babaebe2d59bb3be239851f33c6a1af0a6
Badescu Petre Cosmin <badescupetrecosmin@gmail.com>
2025-03-25 12:15:40 +0200

    add lint fix

--- src/app/api/history/route.ts
+++ src/app/api/history/route.ts
@@ -1,10 +1,8 @@
-import { fetchWithCustomHeaders } from "@/app/libs/headers";
-
-export const GET = async (request: Request) => {
-  const url = new URL(request.url);
-  const symbol = url.searchParams.get("symbol") || "SXRV.DE";
-  const interval = url.searchParams.get("interval") || "1mo";
-  const diffAndSplits = url.searchParams.get("diffandsplits") || "false";
+export const GET = async () => {
+  // const url = new URL(request.url);
+  // const symbol = url.searchParams.get("symbol") || "SXRV.DE";
+  // const interval = url.searchParams.get("interval") || "1mo";
+  // const diffAndSplits = url.searchParams.get("diffandsplits") || "false";
 
   // const response = await fetchWithCustomHeaders(
   //   `https://yahoo-finance15.p.rapidapi.com/api/v1/markets/stock/history?symbol=${symbol}&interval=${interval}&diffandsplits=${diffAndSplits}`
